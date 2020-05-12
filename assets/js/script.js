cdd2b42fcdb57a9bf602855da8b8b91d72cd7ed5
Alastair Huntington <mr4211@pleasantonusd.net>
2020-05-12 20:31:49 +0000

    implemented sticky navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log(jQuery.browser.mobile)
+$(() => {console.log(jQuery.browser.mobile)
+  var navbar = $('.navbar')[0]
+  console.log(navbar)
+  var sticky = navbar.offsetTop;
+  console.log(sticky)
+  window.onscroll = () => {
+    if (window.pageYOffset >= sticky)
+      navbar.classList.add("sticky")
+    else 
+      navbar.classList.remove("sticky");
+}})
